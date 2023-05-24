cf028df99aac45d3a59d4cef7c6307671e83d6ca
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2023-05-24 19:27:44 +0200

    Fixed accordion animations

--- assets/js/utente.js
+++ assets/js/utente.js
@@ -2,10 +2,19 @@ let collapseBtns = document.querySelectorAll('button[data-bs-toggle="collapse"]'
 collapseBtns.forEach(btn => {
 	btn.addEventListener('click', (ev) => {
 		/**@type{ HTMLElement} */
-		let icon = ev.target.querySelector('i')
+		let btn = ev.target
+		let icon = btn.querySelector('i')
 
 		if (icon.style.transform == '') {
 			icon.style.transform = 'rotate(90deg)'
+
+			collapseBtns.forEach(oBtn => {
+				if (oBtn == btn) {
+					return
+				}
+
+				oBtn.querySelector('i').style.transform = ''
+			})
 		} else {
 			icon.style.transform = ''
 		}
